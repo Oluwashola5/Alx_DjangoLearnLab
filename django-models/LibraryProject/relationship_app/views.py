35f92a102cb79d51bed95b175862a52d73dbadf9
Oluwashola <Oluwasholabj@gmail.com>
2025-02-23 18:21:54 +0100

    Ability

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -109,15 +109,15 @@ def custom_render(request, template_name):
 
 @user_passes_test(is_admin)
 def admin_view(request):
-    return custom_render(request, 'admin_view.html')
+    return render(request, 'admin_view.html')
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return custom_render(request, 'librarian_view.html')
+    return render(request, 'librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
-    return custom_render(request, 'member_view.html')
+    return render(request, 'member_view.html')
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
