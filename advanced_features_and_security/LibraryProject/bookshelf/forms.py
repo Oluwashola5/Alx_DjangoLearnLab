d8302e9ce529c71764d135aec9bda213706e8b18
Oluwashola <Oluwasholabj@gmail.com>
2025-03-02 18:16:12 +0100

    Number 3

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -11,3 +11,14 @@ class BookForm(forms.ModelForm):
         if "<script>" in title:  # Basic XSS protection example
             raise forms.ValidationError("Invalid characters in title!")
         return title
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+
+    def clean_name(self):
+        name = self.cleaned_data.get("name")
+        if "<script>" in name:  # Basic XSS protection example
+            raise forms.ValidationError("Invalid characters in name!")
+        return name
