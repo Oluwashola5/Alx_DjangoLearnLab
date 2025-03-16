03e147d58d0e8e414dd79d48fa76b02946d4ef7a
Oluwashola <Oluwasholabj@gmail.com>
2025-03-16 14:18:04 +0100

    Only

---
+++ advanced-api-project/api/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for advanced_api_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import (
+    BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('books/', BookListView.as_view(), name='book-list'),  # List all books
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Get book by ID
+    path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a book
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),  # Update a book
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+]
