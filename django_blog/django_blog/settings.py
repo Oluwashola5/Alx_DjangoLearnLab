5d333799377a42185b2f0a10174ff8a913b10e09
Oluwashola <Oluwasholabj@gmail.com>
2025-03-23 14:25:43 +0100

    Correct database settings

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -80,6 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'your_database_user',  # Add your database username
+        'PASSWORD': 'your_database_password',  # Set your database password
+        'HOST': 'localhost',  # Change this if using an external DB
+        'PORT': '5432',
     }
 }
 
