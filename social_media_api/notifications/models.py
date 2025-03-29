9ac5d40a1543d3f5dfedb8601ca3f1a163c0dea5
Oluwashola <Oluwasholabj@gmail.com>
2025-03-29 17:33:24 +0100

    zells

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+User = get_user_model()
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notifications"
+    )
+    actor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="actor"
+    )
+    verb = models.CharField(max_length=255)  # e.g., "liked your post"
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    target_object_id = models.PositiveIntegerField()
+    target = GenericForeignKey("target_content_type", "target_object_id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.actor} {self.verb} {self.target} - {self.recipient}"
+
 
 # Create your models here.
