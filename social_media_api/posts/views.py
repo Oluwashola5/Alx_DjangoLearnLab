00bd1b5ee8397b63dcaa84a550098c5e396dbe00
Oluwashola <Oluwasholabj@gmail.com>
2025-03-29 16:36:10 +0100

    Posts api

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
@@ -7,19 +7,22 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()  # Explicitly include this
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_pk')
-        return Comment.objects.filter(post_id=post_id).order_by('-created_at')
+        if post_id:
+            return Comment.objects.filter(post_id=post_id).order_by('-created_at')
+        return Comment.objects.all()  # Ensure it returns all comments when no post_id is provided
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-# Create your views here.
